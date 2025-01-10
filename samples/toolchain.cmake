4613c98bd9c122b9a7d57890beb8b969fc3e2365
孔翔宇 <kongxiangyu@Keynection>
2025-01-10 16:45:57 +0800

    toochain

---
+++ samples/toolchain.cmake
@@ -0,0 +1,15 @@
+set(TOOLCHAIN_ROOT "/home/kxy/T23_Tool/ISVP-T23-1.1.2-20240204/software/zh/Ingenic-SDK-T23-1.1.2-20240204-zh/resource/toolchain/gcc_540/mips-gcc540-glibc222-64bit-r3.3.0.smaller")
+
+# 指定交叉编译器路径和相关配置
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR mips)
+
+set(CMAKE_C_COMPILER "${TOOLCHAIN_ROOT}/bin/mips-linux-gnu-gcc")
+set(CMAKE_CXX_COMPILER "${TOOLCHAIN_ROOT}/bin/mips-linux-gnu-g++")
+set(CMAKE_FIND_ROOT_PATH "${TOOLCHAIN_ROOT}")
+
+# 设置搜索路径
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
